023d6c1ee0db8e88a61086d45b344516b317a221
GeorgeAntwanHosny <georgeantwanhosnyghaly@outlook.com>
2023-04-04 00:01:05 +0200

    add team-app project folders

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+ <div class="p-4 pt-4">
+   <TeamView/>
+ </div>
+</template>
+<script setup>
+import TeamView from "@/views/TeamView.vue";
+</script>
---
+++ src/components/Teams/TeamHeader.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { useTeamStore } from "@/stores/TeamStore";
+import Modal from "@/components/Modal.vue";
+import {ref} from "vue";
+
+let team = useTeamStore();
+let show =ref(false)
+</script>
+
+<template>
+  <header class="flex justify-between">
+    <div>
+      <button  class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
+               @click="show=true"
+
+              :disabled="team.members.length === team.spots">Add Member ({{  team.spotsRemaining}} Spots Left)</button>
+    </div>
+    <Modal :show="show"  @close="show=false"/>
+    <div>
+      <div class="inline-flex items-center text-3xl relative">
+        <img src="/smiley.png" alt="" class="mr-2">
+        <h3>{{ team.name }} Team</h3>
+        <div class="bg-green-400 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-4 -top-2">{{ team.spots }}</div>
+      </div>
+    </div>
+
+  </header>
+</template>
